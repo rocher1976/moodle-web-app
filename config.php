6c0dcdc40481be061e662f0f0b7abb6b4037e51a
rocher1976 <126126941+rocher1976@users.noreply.github.com>
2025-01-18 15:58:37 +0000

    made config  changes to config.php file

--- config.php
+++ config.php
@@ -38,12 +38,12 @@ $CFG = new stdClass();
 // will be stored.  This database must already have been created         //
 // and a username/password created to access it.                         //
 
-$CFG->dbtype    = 'pgsql';      // 'pgsql', 'mariadb', 'mysqli', 'auroramysql', 'sqlsrv' or 'oci'
+$CFG->dbtype    = 'mysqli';      // 'pgsql', 'mariadb', 'mysqli', 'auroramysql', 'sqlsrv' or 'oci'
 $CFG->dblibrary = 'native';     // 'native' only at the moment
-$CFG->dbhost    = 'localhost';  // eg 'localhost' or 'db.isp.com' or IP
-$CFG->dbname    = 'moodle';     // database name, eg moodle
-$CFG->dbuser    = 'username';   // your database username
-$CFG->dbpass    = 'password';   // your database password
+$CFG->dbhost    = getenv('AZURE_MYSQL_HOST');  // eg 'localhost' or 'db.isp.com' or IP
+$CFG->dbname    = getenv('AZURE_MYSQL_DBNAME');     // database name, eg moodle
+$CFG->dbuser    = getenv('AZURE_MYSQL_USERNAME');   // your database username
+$CFG->dbpass    = getenv('AZURE_MYSQL_PASSWORD');   // your database password
 $CFG->prefix    = 'mdl_';       // prefix to use for all table names
 $CFG->dboptions = array(
     'dbpersist' => false,       // should persistent database connections be
@@ -162,7 +162,7 @@ $CFG->dboptions = array(
 // If you need both intranet and Internet access please read
 // http://docs.moodle.org/en/masquerading
 
-$CFG->wwwroot   = 'http://example.com/moodle';
+$CFG->wwwroot   = 'https://new-moodle-app-hfc2hxaxgabjcyaa.uksouth-01.azurewebsites.net';
 
 
 //=========================================================================
@@ -178,7 +178,7 @@ $CFG->wwwroot   = 'http://example.com/moodle';
 //
 // - On Windows systems you might specify something like 'c:\moodledata'
 
-$CFG->dataroot  = '/home/example/moodledata';
+$CFG->dataroot  = '/home/moodledata';
 
 // Whether the Moodle router is fully configured.
 //
